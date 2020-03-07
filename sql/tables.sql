c96af85f1a560c8e1ab322f1be431393470dd9c3
Jakub Gurgacz <37517798+zuber966@users.noreply.github.com>
2020-03-07 16:51:07 +0100

    restaurant users table schema

---
+++ sql/tables.sql
@@ -0,0 +1,55 @@
+DROP DATABASE restaurant_users;
+CREATE DATABASE restaurant_users;
+USE restaurant_users;
+
+CREATE TABLE role
+(
+    id       INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    rolename enum ('user', 'admin')
+);
+
+CREATE TABLE address
+(
+    id               INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    street           VARCHAR(128) NOT NULL,
+    postal_code      VARCHAR(6)   NOT NULL,
+    city             VARCHAR(128) NOT NULL,
+    apartment_number SMALLINT     NOT NULL,
+    flat_number      TINYINT DEFAULT NULL
+) CHARACTER SET utf8
+  COLLATE utf8_polish_ci;
+
+CREATE TABLE user_info
+(
+    id           INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    first_name   VARCHAR(128) NOT NULL,
+    last_name    VARCHAR(128) NOT NULL,
+    phone_number INT UNSIGNED NOT NULL,
+    address_id   INT UNSIGNED NOT NULL,
+    FOREIGN KEY (address_id) REFERENCES address (id)
+) CHARACTER SET utf8
+  COLLATE utf8_polish_ci;
+
+CREATE TABLE registration_info
+(
+    id                INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    registration_date DATETIME NOT NULL,
+    hash              VARCHAR(64),
+    activation_date   DATETIME NOT NULL
+);
+
+CREATE TABLE user
+(
+    id                   INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    username             VARCHAR(64)        NOT NULL,
+    password             VARCHAR(64)        NOT NULL,
+    email                VARCHAR(250)       NOT NULL,
+    is_activated         BOOL DEFAULT FALSE NOT NULL,
+    is_enabled           BOOL DEFAULT FALSE NOT NULL,
+    role_id              INT UNSIGNED       NOT NULL,
+    registration_info_id INT UNSIGNED       NOT NULL,
+    user_info_id         INT UNSIGNED       NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (registration_info_id) REFERENCES registration_info (id),
+    FOREIGN KEY (user_info_id) REFERENCES user_info (id)
+);
