84875e56f14cb2869387828089b1a4ba571f7df9
Jakub Gurgacz <37517798+zuber966@users.noreply.github.com>
2020-04-10 17:05:10 +0200

    validator tests

--- register-service/src/test/java/com/vandemos/registerservice/RegisterServiceApplicationTests.java
+++ register-service/src/test/java/com/vandemos/registerservice/RegisterServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.vandemos.registerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RegisterServiceApplicationTests {
 
 	@Test
--- register-service/src/test/java/com/vandemos/registerservice/ValidatorUtilsTests.java
+++ register-service/src/test/java/com/vandemos/registerservice/ValidatorUtilsTests.java
@@ -12,10 +12,10 @@ import static org.junit.Assert.assertTrue;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ValidatorUtilsTests {
 
+    ValidatorUtils validatorUtils = new ValidatorUtils();
+
     @Test
     public void passwordValidatorTest(){
-        ValidatorUtils validatorUtils = new ValidatorUtils();
-
         String password = "zaq1@WSX";
 
         boolean result = validatorUtils.validatePassword(password);
@@ -25,12 +25,60 @@ public class ValidatorUtilsTests {
 
     @Test
     public void passwodValidatorTest_notValidPassword(){
-        ValidatorUtils validatorUtils = new ValidatorUtils();
-
         String notValidPassword = "password";
 
         boolean result = validatorUtils.validatePassword(notValidPassword);
 
         assertFalse(result);
+    }
+
+    @Test
+    public void passwordTheSameTest(){
+        String pass = "somePass";
+        String pass2 = "somePass";
+        assertTrue(validatorUtils.passwordsTheSame(pass, pass2));
+    }
+
+    @Test
+    public void passwordNotTheSameTest(){
+        String pass = "somePass";
+        String pass2 = "somePass1";
+        assertFalse(validatorUtils.passwordsTheSame(pass, pass2));
+    }
+
+    @Test
+    public void emailValidationTest(){
+        String validEmail = "mail.asf23_sf@dd2.com";
+        assertTrue(validatorUtils.validateEmail(validEmail));
+    }
+
+    @Test
+    public void emailValidationTest_emailNotValid(){
+        String notValidEmail = "afff@@d3.pl";
+        assertFalse(validatorUtils.validateEmail(notValidEmail));
+    }
+
+    @Test
+    public void usernameValidationTest(){
+        String validUsername = "username12_";
+        assertTrue(validatorUtils.validateUsername(validUsername));
+    }
+
+    @Test
+    public void usernameValidationTest_usernameNotValid(){
+        String validUsername = "userna&%me12_";
+        assertFalse(validatorUtils.validateUsername(validUsername));
+    }
+
+    @Test
+    public void phoneNumberValidationTest(){
+        Integer validPhoneNumber = 123456789;
+        assertTrue(validatorUtils.validatePhoneNumber(validPhoneNumber));
+    }
+
+    @Test
+    public void phoneNumberValidationTest_phontNumberNotValid(){
+        Integer notValidPhoneNumber = 1234567890;
+        assertFalse(validatorUtils.validatePhoneNumber(notValidPhoneNumber));
     }
 }
